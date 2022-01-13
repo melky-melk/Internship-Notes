c6c72ff22285dfbcc62208b4b5dc72566203f7e5
melky-melk <84921717+melky-melk@users.noreply.github.com>
2022-01-13 11:55:43 +1100

    moving files and changing names

--- Python/4SavingData.py
+++ Python/4SavingData.py
@@ -5,3 +5,5 @@ df = pd.read_csv('datasets/Pokemon.csv')
 df.to_csv('datasets/modified.csv')
 
 new_df = df.loc[(df['Type 1'] == 'Grass') | (df['Type 2'] == 'Poison')]
+
+new_df.to_csv('datasets/filtered.csv')
